60643292fd1dd740a33f33707061afca5ae73147
TechSamQC <samuelbrassard86@gmail.com>
2025-09-24 17:22:20 -0400

    Mise a jour classe ticket pour verifier la saisie du statut

--- src/Ticket.java
+++ src/Ticket.java
@@ -22,6 +22,7 @@ public class Ticket {
         this.priority = priority;
         this.creationDate = LocalDate.now().toString();
         this.updateDate = LocalDate.now().toString();
+        this.assignedUserId = 0; // 0 signifie non assigné
         this.comments = new ArrayList<>();
     }
 
@@ -104,13 +105,36 @@ public class Ticket {
         System.out.println("Ticket " + ticketID + " assigné à l'utilisateur " + this.assignedUserId + ".");
     }
 
-    public void updateStatus(String status) {
-        // Mettre à jour le statut du ticket
-        //SI le statut n'est pas valide, il faut afficher un message d'erreur
-        if (!status.equals("OUVERT") && !status.equals("ASSIGNÉ") &&
+    public void updateStatus(String status) { // Mettre à jour le statut du ticket
+        //SI le ticket est terminé, il n'est pas dans un état permettant la modification
+        if (this.status.equals("TERMINÉ")) {
+                System.out.println("Impossible de faire cette manipulation, le ticket est terminé.\n");
+                return;
+        }
+        //SI le statut saisi n'est pas valide, il faut afficher un message d'erreur
+        else if (!status.equals("OUVERT") && !status.equals("ASSIGNÉ") &&
          !status.equals("VALIDATION") && !status.equals("TERMINÉ")) {
             System.out.println("Statut invalide. Le statut doit être l'un des suivants : OUVERT, ASSIGNÉ, VALIDATION, TERMINÉ. \n");
             return;
+        }
+        //SI le ticket est remis à ouvert, il est désassigné automatiquement
+        else if (status.equals("OUVERT")) {
+            if (assignedUserId != 0) {
+                assignedUserId = 0;
+                System.out.println("Le ticket " + ticketID + " est maintenant ouvert et a été automatiquement désassigné de l'utilisateur " + assignedUserId + ".\n");
+                return;
+            }
+            else {
+                System.out.println("Le ticket " + ticketID + " était déjà ouvert.\n");
+                return;
+            }
+        }
+        //SI le ticket n'est pas assigné, il ne peut pas être mis en validation
+        else if (status.equals("VALIDATION")) {
+            if (!this.status.equals("ASSIGNÉ")) {
+                System.out.println("Un ticket ne peut être mis en validation que s'il est assigné d'abord.\n");
+                return;
+            }
         }
         //SI le ticket n'est pas en validation, il ne peut pas être terminé
         else if (status.equals("TERMINÉ")) {
@@ -118,12 +142,8 @@ public class Ticket {
                 System.out.println("Un ticket ne peut être terminé que s'il a été validé d'abord.\n");
                 return;
             }
-            }
-        //SI le ticket n'est pas dans un état permettant la modification
-        else if (this.status.equals("TERMINÉ")) {
-                System.out.println("Impossible de faire cette manipulation, le ticket est terminé.\n");
-                return;
-            }
+        }
+        
         this.status = status;
         this.updateDate = LocalDate.now().toString();
         System.out.println("Le statut du ticket " + ticketID + " a été mis à jour à : " + status);
