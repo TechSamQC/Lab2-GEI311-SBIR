1eb7b0d96ddd682ee2f7ce85095b60898bd73381
IL-aina-Ratefinanahary <ratefilaina@gmail.com>
2025-11-19 01:03:24 -0500

    Mise à jour Des model pour enlever les warnings

--- server_springboot/src/main/java/org/openapitools/model/TicketDTO.java
+++ server_springboot/src/main/java/org/openapitools/model/TicketDTO.java
@@ -1,6 +1,5 @@
 package org.openapitools.model;
 
-import java.net.URI;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -10,17 +9,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.openapitools.jackson.nullable.JsonNullable;
-import org.openapitools.model.Description;
 import org.springframework.format.annotation.DateTimeFormat;
-import java.util.NoSuchElementException;
-import org.openapitools.jackson.nullable.JsonNullable;
-import java.time.OffsetDateTime;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-
-import java.util.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import javax.annotation.Generated;
 
 /**
